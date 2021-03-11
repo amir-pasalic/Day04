7392ee69ab910d70ee054e81da262a81448972af
amir-pasalic <amirpasalic144@gmail.com>
2021-03-12 00:18:09 +0100

    Assignment done !

---
+++ ex10/src/index.js
@@ -0,0 +1,18 @@
+function main() {
+  // Only change code below this line
+  var myFirstName = "Amir";
+  var myLastName = "Pasalic";
+  var myHomeTown = "Bosanska Krupa";
+
+  // Only change code above this line
+
+  return {
+    myFirstName,
+    myLastName,
+    myHomeTown,
+  };
+}
+
+console.log(main());
+
+module.expoerts = main;
